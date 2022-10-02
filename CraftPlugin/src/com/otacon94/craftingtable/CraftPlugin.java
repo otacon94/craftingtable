5948e8fe080ebfe862e6f2cb0e52e0dfd530eda2
LetFlo <92692202+LetFlo@users.noreply.github.com>
2022-10-02 19:10:28 +0200

    Update CraftPlugin.java
    removed ct reference

--- CraftPlugin/src/com/otacon94/craftingtable/CraftPlugin.java
+++ CraftPlugin/src/com/otacon94/craftingtable/CraftPlugin.java
@@ -8,13 +8,11 @@ import com.otacon94.listeners.CraftingListener;
 public class CraftPlugin extends JavaPlugin{
 
 	private static final String COMMAND1 = "wb";
-	private static final String COMMAND2 = "ct";
 	
 	@Override
 	public void onEnable() {
 		getLogger().info("OtaconCraftPlugin is now enabled");
 		getCommand(COMMAND1).setExecutor(new CraftingListener());
-		getCommand(COMMAND2).setExecutor(new CraftingListener());
 	}
 	
 	
